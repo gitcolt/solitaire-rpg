aa1a63274fe6cb3f56a3481211f95d8687deeb86
colt <colten@utexas.edu>
2022-05-21 19:00:18 -0500

    Move common code out of derived classes up into Entity class

--- src/entity.ts
+++ src/entity.ts
@@ -1,5 +1,14 @@
 import playerTextureAtlasPath from './assets/player.png';
 import {Tile, TILE_SIZE} from './overworld';
+import {tileToScreenPos} from './utils';
+
+// EntitySpeed.VALUE is the amount of a TILE_SIZE that the Entity traverses
+// in a single frame
+enum EntitySpeed {
+  SLOW = 0.05,
+  MED = 0.1,
+  FAST = 0.15,
+}
 
 export abstract class Entity {
   currTile: Tile;
@@ -8,7 +17,7 @@ export abstract class Entity {
   tileOffsetX: number;
   tileOffsetY: number;
   isWalking: boolean;
-  speed: number;
+  speed: EntitySpeed;
   angle: number;
   textureAtlas: HTMLImageElement;
   textureAtlasIdx: number;
@@ -20,8 +29,8 @@ export abstract class Entity {
     this.destTile = null;
     this.t = 0;
     this.isWalking = false;
-    this.speed = TILE_SIZE / 8;
-    this.angle = 0;
+    this.speed = EntitySpeed.SLOW;
+    this.angle = Math.PI / 2;
     this.textureAtlas = null;
     this.textureAtlasIdx = 0;
 
@@ -36,7 +45,7 @@ export abstract class Entity {
     if (!this.currTile || !this.destTile)
       return;
     if (this.currTile != this.destTile) {
-      this.t += 0.04;
+      this.t += this.speed;
       this.tileOffsetX = this.t * (this.destTile.x - this.currTile.x);
       this.tileOffsetY = this.t * (this.destTile.y - this.currTile.y);
       if (this.t >= 1) {
@@ -70,6 +79,35 @@ export abstract class Entity {
   }
 
 
-  abstract render(ctx: CanvasRenderingContext2D,
-                  screenOffsetX: number, screenOffsetY: number): void
+  render(ctx: CanvasRenderingContext2D, screenOffsetX: number, screenOffsetY: number) {
+    if (!this.textureAtlas)
+      return;
+
+    let [screenPosX, screenPosY] = tileToScreenPos(this.currTile);
+    screenPosX += this.tileOffsetX * TILE_SIZE - screenOffsetX;
+    screenPosY += this.tileOffsetY * TILE_SIZE - screenOffsetY;
+
+    if (window.debug) {
+      ctx.beginPath();
+      ctx.lineWidth = 4;
+      ctx.strokeStyle = 'red';
+      ctx.arc(screenPosX + TILE_SIZE/2, screenPosY + TILE_SIZE/2, 10, 0, 2*Math.PI);
+      ctx.stroke();
+      ctx.moveTo(screenPosX + TILE_SIZE/2, screenPosY + TILE_SIZE/2);
+      ctx.lineTo(screenPosX + TILE_SIZE/2 + Math.cos(this.angle) * 20,
+                 screenPosY + TILE_SIZE/2 + Math.sin(this.angle) * 20);
+      ctx.stroke();
+      return;
+    }
+
+    ctx.drawImage(this.textureAtlas,
+                  this.textureAtlasIdx * this.textureAtlas.height,
+                  0,
+                  this.textureAtlas.height,
+                  this.textureAtlas.height,
+                  screenPosX,
+                  screenPosY,
+                  TILE_SIZE,
+                  TILE_SIZE);
+  }
 }
--- src/player.ts
+++ src/player.ts
@@ -1,6 +1,4 @@
-import {TILE_SIZE} from './overworld';
 import {Entity} from './entity';
-import {tileToScreenPos} from './utils';
 
 export class Player extends Entity {
   xp: number;
@@ -8,37 +6,5 @@ export class Player extends Entity {
   constructor() {
     super();
     this.xp = 0;
-  }
-
-  render(ctx: CanvasRenderingContext2D, screenOffsetX: number, screenOffsetY: number) {
-    if (!this.textureAtlas)
-      return;
-
-    let [screenPosX, screenPosY] = tileToScreenPos(this.currTile);
-    screenPosX += this.tileOffsetX * TILE_SIZE - screenOffsetX;
-    screenPosY += this.tileOffsetY * TILE_SIZE - screenOffsetY;
-
-    if (window.debug) {
-      ctx.beginPath();
-      ctx.lineWidth = 4;
-      ctx.strokeStyle = 'red';
-      ctx.arc(screenPosX + TILE_SIZE/2, screenPosY + TILE_SIZE/2, 10, 0, 2*Math.PI);
-      ctx.stroke();
-      ctx.moveTo(screenPosX + TILE_SIZE/2, screenPosY + TILE_SIZE/2);
-      ctx.lineTo(screenPosX + TILE_SIZE/2 + Math.cos(this.angle) * 20,
-                 screenPosY + TILE_SIZE/2 + Math.sin(this.angle) * 20);
-      ctx.stroke();
-      return;
-    }
-
-    ctx.drawImage(this.textureAtlas,
-                  this.textureAtlasIdx * this.textureAtlas.height,
-                  0,
-                  this.textureAtlas.height,
-                  this.textureAtlas.height,
-                  screenPosX,
-                  screenPosY,
-                  TILE_SIZE,
-                  TILE_SIZE);
   }
 }
